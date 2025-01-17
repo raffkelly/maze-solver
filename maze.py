0d6e3e0833d161f8f282165c17cc6194293bda1c
raffkelly <raffkelly@gmail.com>
2025-01-16 22:01:48 -0500

    full qol and bfs/dfs options

--- maze.py
+++ maze.py
@@ -55,11 +55,11 @@ class Maze:
             return
         self._win.redraw()
         if type == "undo":
-            time.sleep(.25)
+            time.sleep(.1)
         elif type == "maze":
             time.sleep(.01)
         elif type == "solve":
-            time.sleep(.1)
+            time.sleep(.05)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
@@ -143,3 +143,47 @@ class Maze:
                 self._animate("undo")
                 self._cells[i][j].draw_move(self._cells[i][j+1], True)
         return False
+    
+    def solve_bfs(self):
+        self._solve_r_bfs(0, 0)
+    
+    def _solve_r_bfs(self, i, j):
+        visited = []
+        to_visit = []
+        to_visit.append((i,j))
+        path = {(i, j): None}
+        while to_visit:
+            current = to_visit.pop(0)
+            i = current[0]
+            j = current[1]
+            visited.append(current)
+            if current == (self._num_cols -1, self._num_rows -1):
+                path_list = []
+                while current is not None:
+                    path_list.append(current)
+                    current = path[current]
+                path_list.reverse()
+                break
+            if i > 0 and self._cells[i][j].has_left_wall == False and (i-1, j) not in visited and (i-1, j) not in to_visit:
+                to_visit.append((i-1, j))
+                path[(i-1, j)] = current
+            if i < (self._num_cols - 1) and self._cells[i][j].has_right_wall == False and (i+1, j) not in visited and (i+1, j) not in to_visit:
+                to_visit.append((i+1, j))
+                path[(i+1, j)] = current          
+            if j > 0 and self._cells[i][j].has_top_wall == False and (i, j-1) not in visited and (i, j-1) not in to_visit:
+                to_visit.append((i, j-1))
+                path[(i, j-1)] = current          
+            if j < (self._num_rows - 1) and self._cells[i][j].has_bottom_wall == False and (i, j+1) not in visited and (i, j+1) not in to_visit:
+                to_visit.append((i, j+1))
+                path[(i, j+1)] = current
+        for i in range(len(path_list)):
+            if i == len(path_list) - 1:
+                return
+            current_i = path_list[i][0]
+            current_j = path_list[i][1]
+            next_i = path_list[i+1][0]
+            next_j = path_list[i+1][1]
+            current_cell = self._cells[current_i][current_j]
+            next_cell = self._cells[next_i][next_j]
+            current_cell.draw_move(next_cell)
+            self._animate("maze")
